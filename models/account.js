185db433c14c20fd9962255fa4e09d3cc40e2653
Khoa Le <lowji32101@gmail.com>
2022-05-14 17:33:54 +0700

    sua trang resetPassword

--- models/account.js
+++ models/account.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const account = new mongoose.Schema({
     sdt: {type: String, unique: true},
-    quyen: String,
+    quyen: String, //0 admin,  1 chờ kích hoạt, 2 được kích hoạt, 3 bị hủy 
     username: String,
     password: String,
     tenNguoiDung: String,
@@ -13,9 +13,9 @@ const account = new mongoose.Schema({
     email: {type: String, unique: true},
     ngayMoThe: String,
     soDu: { type: Number, default: 0 },
-    loi: { type: Number, default: 0 },
-    needResetPassword: { type: Boolean, default: true },
-    timeBeUnlocked: Date,
+    loi: { type: Number, default: 0 }, //3 loi khoa 1 phut, 6 loi khoa vinh vien
+    needResetPassword: { type: Boolean, default: true }, //true => can reset mat khau khi moi tao acc
+    timeBeUnlocked: Date, //thoi gian duoc mo khoa (1 phut sau khi sai mk 3 lan)
 }, { collection: 'account', versionKey: false });
 
 const accountModel = mongoose.model('account',account);
